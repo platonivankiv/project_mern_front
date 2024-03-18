f13cb228f816b849531ed0fa01889abc45981999
platon.ivankiv <ivankiv.platon@gmail.com>
2024-03-18 17:10:52 +0300

    added functions for authorization and saving token in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import Container from "@mui/material/Container";
 import {Route, Routes} from "react-router-dom";
 import {Header} from './components/Header'
 import {Home, FullPost, Registration, Login, AddPost} from "./pages";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {fetchAuthMe, selectIsAuth} from "./redux/slices/auth";
 
 export function App() {
+    const dispatch = useDispatch()
+    const isAuth = useSelector(selectIsAuth);
+
+
+    useEffect(() => {
+        dispatch(fetchAuthMe())
+    }, []);
+
     return (
         <>
             <Header/>
             <Container maxWidth='lg'>
-
-
                 <Routes>
-
                     <Route path="/" element={<Home/>}/>
                     <Route path="/posts/:id" element={<FullPost/>}/>
                     <Route path="/add-post" element={<AddPost/>}/>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,11 +4,17 @@ import Button from '@mui/material/Button';
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
 import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {logout, selectIsAuth} from "../../redux/slices/auth";
 
 export const Header = () => {
-    const isAuth = false;
+    const dispatch = useDispatch()
+    const isAuth = useSelector(selectIsAuth);
 
     const onClickLogout = () => {
+        if (window.confirm('Вы действительно хотите выйти?'))
+        dispatch(logout())
+        window.localStorage.removeItem('token')
     };
 
     return (
